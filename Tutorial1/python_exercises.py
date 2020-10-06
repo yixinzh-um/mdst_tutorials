e61f4ec2a400ab886e2667139320f2f1bb14ebb9
Yixin Zheng <yixinzh@umich.edu>
2020-10-06 15:19:13 -0400

    tutorial1

--- Tutorial1/python_exercises.py
+++ Tutorial1/python_exercises.py
@@ -6,13 +6,21 @@ def is_odd(x):
     """
     returns True if x is odd and False otherwise
     """
+    if x % 2 == 1:
+        return True
+    else:
+        return False    
 
 
 def is_palindrome(word):
     """
     returns whether `word` is spelled the same forwards and backwards
     """
-
+    if word == word[::-1]:
+        return True
+    else:
+        return False
+    
 
 def only_odds(numlist):
     """
@@ -20,6 +28,11 @@ def only_odds(numlist):
 
     ex: only_odds([1, 2, 3, 4, 5, 6]) -> [1, 3, 5]
     """
+    lst=[]
+    for i in numlist:
+        if i%2==1:
+            lst.append(i)
+    return lst
 
 
 def count_words(text):
@@ -35,3 +48,12 @@ def count_words(text):
         {'how': 1, 'much': 1, 'wood': 1, 'would': 1, 'a': 2, 'woodchuck': 2,
         'chuck': 2, 'if': 1, 'could': 1, 'wood?': 1}
     """
+    dict={}
+    words=text.split(" ")
+    for word in words:
+        word_l=word.lower()
+        if word_l not in dict:
+            dict[word_l]=1
+        else:
+            dict[word_l] += 1
+    return dict
