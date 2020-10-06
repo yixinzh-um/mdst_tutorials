258a52fb6e66fa1b0adba795854e285a60190ade
Yixin Zheng <yixinzh@umich.edu>
2020-10-06 15:11:03 -0400

    tutorial 1,2

---
+++ Tutorial1/python_exercises.py
@@ -0,0 +1,37 @@
+"""
+Intro to python exercises shell code
+"""
+
+def is_odd(x):
+    """
+    returns True if x is odd and False otherwise
+    """
+
+
+def is_palindrome(word):
+    """
+    returns whether `word` is spelled the same forwards and backwards
+    """
+
+
+def only_odds(numlist):
+    """
+    returns a list of numbers that are odd from numlist
+
+    ex: only_odds([1, 2, 3, 4, 5, 6]) -> [1, 3, 5]
+    """
+
+
+def count_words(text):
+    """
+    return a dictionary of {word: count} in the text
+
+    words should be split by spaces (and nothing else)
+    words should be converted to all lowercase
+
+    ex: count_words("How much wood would a woodchuck chuck"
+                    " if a woodchuck could chuck wood?")
+        ->
+        {'how': 1, 'much': 1, 'wood': 1, 'would': 1, 'a': 2, 'woodchuck': 2,
+        'chuck': 2, 'if': 1, 'could': 1, 'wood?': 1}
+    """
